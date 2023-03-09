ee795835be5846bbc6a0c74f99c0da14f1b00501
Tolu A <mrtalder@gmail.com>
2023-03-09 10:11:33 +0000

    Change grid size from user input

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 //Declaration of DOMs
 const container = document.getElementById('container');
 let divTemplate = document.createElement('div');
-let gridButton = document.getElementById('gridButton')
+let gridButton = document.getElementById('gridButton');
+let numberOfSquares;
+
 
 //Determine Viewport size
 let viewportWidth = window.innerWidth;
@@ -9,7 +11,7 @@ let viewportHeight = window.innerHeight;
 let squareSize = 16;
 let squareHorizontal = Math.floor(viewportWidth / squareSize);
 let squareVertical = Math.floor(viewportHeight / squareSize);
-let squareCount = squareHorizontal* squareVertical;
+let squareCount;
 
 //Create Container box
 container.style.display = 'flex';
@@ -17,19 +19,67 @@ container.style.flexWrap = 'wrap';
 container.style.width = viewportWidth + 'px';
 container.style.height = viewportHeight + 'px';
 container.style.overflow = 'hidden';
+container.style.margin = 'auto';
 
 // Create a pattern of squares that fits the window
-for (let i = 0; i < squareCount; i++){
-    let divTemplate = document.createElement('div');
-    divTemplate.style.borderColor = 'red';
-    divTemplate.style.width = '16px';
-    divTemplate.style.height = '16px';
-    divTemplate.style.margin = '0.5px';
-    divTemplate.style.borderStyle = 'solid';
-    container.appendChild(divTemplate);
-    
-    //grid changes colour when you mouse over it
-    divTemplate.addEventListener("mouseover", () => {
-        divTemplate.style.backgroundColor = 'red'
-    })
+function initialGrid (squareCount) {
+    squareCount = squareHorizontal* squareVertical;
+    for (let i = 0; i < squareCount; i++){
+        let divTemplate = document.createElement('div');
+        divTemplate.style.borderColor = 'red';
+        divTemplate.style.width = '16px';
+        divTemplate.style.height = '16px';
+        divTemplate.style.margin = '0.5px';
+        divTemplate.style.borderStyle = 'solid';
+        container.appendChild(divTemplate);
+        
+        //grid changes colour when you mouse over it
+        divTemplate.addEventListener("mouseover", () => {
+            divTemplate.style.backgroundColor = 'red'
+        })
+    }
 }
+
+window.onload = initialGrid();
+
+function userInput () {
+    numberOfSquares = prompt("number of squares per side? (max number = 100)");
+    console.log(numberOfSquares);
+    if (numberOfSquares > 100){
+        while (numberOfSquares > 100){
+            numberOfSquares = prompt("please enter another number");
+        }
+    }
+    return numberOfSquares;  
+}
+
+function removeGrid () {
+    while (container.lastElementChild){
+        container.removeChild(container.lastChild);
+    }
+    // container.replaceChildren();
+}
+
+
+
+gridButton.addEventListener("click", () => {
+    userInput();
+    removeGrid();
+    
+    //Create new grid;
+    squareHorizontal = Math.floor(numberOfSquares);
+    squareVertical = Math.floor(numberOfSquares);
+    squareCount = squareHorizontal* squareVertical;
+    if (numberOfSquares < 20){
+        container.style.minWidth = 'auto';
+        container.style.maxWidth = '256px';
+    }
+    else {
+        container.style.minWidth = 'auto';
+        container.style.maxWidth = squareCount + 'px';
+    }
+    container.style.height = 'auto'
+    container.style.margin = 'auto';
+    initialGrid(squareCount);
+
+})
